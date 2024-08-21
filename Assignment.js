f51413e7747c678e7b9d77019e8db9ecbe4316ab
Faris <farissharayri@gmail.com>
2024-08-20 22:24:48 -0400

    completes exercises 10-11

--- Assignment.js
+++ Assignment.js
@@ -97,3 +97,42 @@ pizza==='tasty'? console.log('yum'):console.log('yuck')
 console.log(pizza==='tasty'?'yum':'yuck')
 
 //#10
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+const LANG = localLangConfig || 'en'
+// Log the result
+console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+const USER_THEME = userSavedTheme || 'light'
+// Log the result
+console.log('User theme setting:', USER_THEME);
+
+
+//#11
+// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
+
+const adventurer = {
+  name: 'Alice',
+};
+
+let cat; // Your code here
+cat = adventurer.cat?.age 
+console.log(cat);
